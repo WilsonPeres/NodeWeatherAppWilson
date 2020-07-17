dfbe1cbab1ef7d7bb5338279cd5e03a531fc5476
LeCoding-tech <59509041+LeCoding-tech@users.noreply.github.com>
2020-07-17 11:09:20 -0400

    fixing error

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ app.use(bodyParser.urlencoded({ extended : true }));
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {weather: null, error: null});
 })
 
 app.post('/', (req, res) => {
     let city = req.body.city
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-    
+
     request(url, (err, response, body) => {
         if (err) {
             res.render('index', { weather : null, error : 'Error, please try again' })
         }
         else {
             let weather = JSON.parse(body)
-            
+
             if(weather.main == undefined) {
                 res.render(res.render('index', { weather : null, error : 'Error, please try again' }))
 
@@ -34,6 +34,8 @@ app.post('/', (req, res) => {
         }
     })
 })
+
+
 
 
 // request(url, (err, response, body) => {
@@ -53,3 +55,4 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`This server is running on port ${port}`)
 })
+
